4f80e82c8f4ec4ebf5e903b32579631b77a7b059
jun <doglovefalcon@gmail.com>
2014-07-17 23:05:15 +0900

    edit a_month_shift html(remove nonsense class&id) js(change total ok)

--- shift_maker/static/js/a_month_shift.js
+++ shift_maker/static/js/a_month_shift.js
@@ -36,18 +36,19 @@ $(document).ready(function(e){
 	});
 
 	$('.toggle_switch').click(function(){
-		//$('.dropdown-menu:visible').hide();
 		var dropdown_menu = $(this.children[0].children[1]);
 		dropdown_menu.show();
 	});
 
 	$('.worktime_choices').click(function(){
+		var staff_id = $(this).attr('staff_id');
+		var worktime_id = $(this).attr('worktime_id');
 		$.ajax({
 			'url':'edit/',
 			'type':'POST',
 			'data':{
-				'staff_id':$(this).attr('staff_id'),
-				'worktime_id':$(this).attr('worktime_id'),
+				'staff_id':staff_id,
+				'worktime_id':worktime_id,
 				'year':$(this).attr('year'),
 				'month':$(this).attr('month'),
 				'day':$(this).attr('day'),
@@ -55,15 +56,10 @@ $(document).ready(function(e){
 			'dataType':'text',
 			'success':function(resp){
 				$('div:has(.dropdown-menu:visible) > a > strong').text(resp);
+				var total_count = $('.table_in[staff_id="' + staff_id + '"]' + '[worktime_id=' + worktime_id + ']');
+				total_count.text( parseInt(total_count.text()) + 1 );
 			},
 		});		  
 	});
 
-	/*var s_count = $('tbody>tr>th').length;
-
-	// 定義した内容をセット
-	for(var i=1;i<=d_count;i++){
-		var d_num = i.toString();
-		setVirtical(d_num);
-	}*/
 });
